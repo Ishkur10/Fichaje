cdb499ec88e8f9c275443ad7c2770d55d0d8ead2
Ilias <iliasouazani@gmail.com>
2025-05-19 17:14:08 +0200

    Revert "upload them timers"
    This reverts commit 05e53bbc3997e26cf9bea996529564c916a813a8.

--- src/context/FichajeContext.jsx
+++ src/context/FichajeContext.jsx
@@ -443,12 +443,6 @@ const togglePausaSesion = (pausar) => {
       console.log(`Editando fichaje ${fichajeId} a fecha ${nuevaFecha}`);
 
       if (sesionActiva && sesionActiva.id === fichajeId) {
-        const originalStartTime = new Date(sesionActiva.fechaInicio);
-         const timeDiffSeconds = (originalStartTime - nuevaFecha) / 1000;
-         const currentTime = tiempoSesion || 0;
-        const newAccumulatedTime = Math.max(0, currentTime + timeDiffSeconds);
-
-
         const nuevaSesion = {
           ...sesionActiva,
           fechaInicio: nuevaFecha.toISOString(),
@@ -457,7 +451,7 @@ const togglePausaSesion = (pausar) => {
         };
         
         setSesionActiva(nuevaSesion);
-        setTiempoSesion(newAccumulatedTime);
+        setTiempoSesion(0);
         
         fichajeService.setSesionActiva(nuevaSesion);
         
